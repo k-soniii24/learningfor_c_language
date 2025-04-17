04151af5e1d73ba03ee225d77005736a7b912d5d
k-soniii24 <khushbusoni2409@gmail.com>
2025-04-17 05:57:00 -0700

    Add files via upload

---
+++ my_first_day.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    printf("hello\n");
+    int age;
+    age = 19.5;
+    printf("%d\n",age);
+    float raju = 18.5;
+    printf("%f\n", raju);
+    char b = 's';
+    printf("%c\n", b);
+   int num_dec = 255;
+   printf("Number with Decimal format is :%d\n",num_dec);
+   int num_oct = 1525;
+   printf("Number with Octal format is :%o\n", num_oct);
+   int num_hex = 1252;
+   printf("Number with Hexa-Decimal format is: %x\n",num_hex);
+}
